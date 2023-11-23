f26ce49371cc6544772b8cf8902e1e1e8c2ce09b
Irina Scortar <irinascortar@gmail.com>
2023-11-23 15:06:27 +0200

    removed unnecessary method in ST for lab4

--- lab4/src/main/java/org/example/SymbolTable.java
+++ lab4/src/main/java/org/example/SymbolTable.java
@@ -10,10 +10,6 @@ public class SymbolTable {
         hashTable = new HashTable(size);
     }
 
-    public Pair findPositionOfTerm(String term) {
-        return hashTable.findPositionOfTerm(term);
-    }
-
     public boolean add(String term) {
         return hashTable.add(term);
     }
